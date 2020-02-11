a52da6694c8e8be0e9af248111f60daadaafbb1b
Rakinne Foote <rakinnebfoote@gmail.com>
2020-02-11 01:14:08 -0800

    Add Bootstrap, styling,  and better Twitter functionality

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const nba = require('nba-api-client');
 const Player = require('./public/js/player.js');
+const Twitter = require('twitter');
+const config = require('./config.js');
 
 const app = express();
+const twitter = new Twitter(config);
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -18,18 +21,32 @@ app.get('/', (req, res) => {
 });
 
 app.post('/player', (req, res, next) => {
+	// getting player name and id from '/' route
 	const playerName = `${req.body.firstname} ${req.body.lastname}`;
 	const getPlayerID = nba.getPlayerID(playerName);
 	const playerID = getPlayerID.PlayerID;
 	const player = new Player(playerName, playerID);
 
-	console.log(nba.getPlayerHeadshotURL(playerName));
-	console.log(playerID);
-	console.log(player.ballerImg());
+	const get = twitter.get('search/tweets', { q: playerName }, (err, tweets, response) => {
+		for (i = 0; i < tweets.statuses.length; i++) {
+			if (!err) {
+				const twitAr = [];
+				twitAr.push(tweets.statuses[i].text);
+			} else {
+				console.log(err);
+			}
+		}
+	});
 
-	res.render('players', { name: playerName, playerImg: player.ballerImg() });
+	res.render('players', {
+		name: playerName,
+		playerImg: player.ballerImg(),
+		tweets: tweetArr
+	});
+
+	// TODO: fix all this shit lol
 });
 
 app.listen(port, () => {
-	console.log('now listening on port ' + port);
+	console.log(`app now listening on ${port}`);
 });
---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	consumer_key: process.env.TWITTER_KEY,
+	consumer_secret: process.env.TWITTER_SECRET,
+	access_token_key: process.env.TWITTER_TOKEN,
+	access_token_secret: process.env.TWITTER_TOKEN_SECRET
+};
